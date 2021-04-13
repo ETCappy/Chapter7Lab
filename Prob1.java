6cf6a525541c472272f3aecf7fae2a5766ad5f38
ECHS Marist College/Newburgh Free Academy <>
2021-04-13 19:28:02 +0000

    Initial commit

---
+++ Prob1.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Prob1{
+  public static void main(String[] args){
+    Scanner scan = new Scanner(System.in);
+    int[] arr = new int(3);
+    arr[1] = scan.nextInt();
+    arr[2] = scan.nextInt();
+    arr[3] = scan.nextInt;
+    System.out.println("Contents: " + arr{1} + " " + arr{2} + " " + arr{3});
+    System.out.println("Sum: " + arr[1] + arr[2] + arr[3]);
+  }
+}
