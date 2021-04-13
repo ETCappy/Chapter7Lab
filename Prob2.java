de61a743a2200cd5c8358f6ccb31d0887f04df2d
ECHS Marist College/Newburgh Free Academy <>
2021-04-13 20:45:35 +0000

    Prob changes

--- Prob2.java
+++ Prob2.java
@@ -1,17 +1,19 @@
 
-public class Prob2  {
-  public static void main(String[] args) {
-    int[] values = { 6, 2, 1, 7, 12, 5 };
-    // Can this loop increment the values?
-// Not as a for each loop because local var is incremented, but it can as a for loop
-    for (int i = 0; i < values.length; i++) {
-      values[i]++;
-      System.out.println("New val: " + values[i]);
-    }
-    // Print out array to see if they really changed
-    System.out.println("Array after the loop: ");
-    for (int i = 0; i < values.length; i++) {
-      System.out.print(values[i] + " ");
-    }
-  }
-}
+public class Prob2
+{
+   public static void main(String[] args){
+     
+   
+       // If element is not evenly divisible by 2 (ie uneven), 
+       // -->increase Count by 1.
+     
+       
+   
+   // Print number of elements with odd values
+ 
+} 
+
+
+     
+   }
+
--- Prob3.java
+++ Prob3.java
@@ -1,8 +1,5 @@
 public class Prob3{
    public static void main(String[] args) {
-      int [] arrEx = {22, 8, 97, 3};
-      int lenArr = arrEx.length; 
-
-      System.out.println("The length of arrEx is: " + lenArr); 
+      
    }
 }
